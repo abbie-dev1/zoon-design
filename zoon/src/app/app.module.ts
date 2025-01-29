8ee60beacd883cf828243fe0fa9e72dad74b80b0
Abbie <sitholeabbie@gmail.com>
2025-01-29 07:17:21 +0200

    Component Created

--- zoon/src/app/app.module.ts
+++ zoon/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
